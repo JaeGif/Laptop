a682e317e1bd77d35d4f303ef2d8e63e988cc3eb
Jae <giffordjacob0@gmail.com>
2024-07-10 10:28:00 -0700

    finished the basic laptop

--- 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -3,6 +3,9 @@ import {
   Environment,
   Float,
   PresentationControls,
+  ContactShadows,
+  Html,
+  Text,
 } from '@react-three/drei';
 
 export default function Experience() {
@@ -14,11 +17,48 @@ export default function Experience() {
       <Environment preset='city' />
       <color args={['#241a1a']} attach='background' />
 
-      <PresentationControls global>
-        <Float rotationIntensity={0.4}>
-          <primitive position-y={-1.2} object={computer.scene} />
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.7}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={'#010101'}
+            rotation={[0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+
+          <primitive position-y={-1.2} object={computer.scene}>
+            <Html
+              transform
+              wrapperClass='htmlScreen'
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src={'https://jgifford.dev/'} />
+            </Html>
+          </primitive>
+          <Text
+            fontSize={0.8}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            textAlign='center'
+            font='bangers-v20-latin-regular.woff'
+          >
+            Hi, I'm Jake!
+          </Text>
         </Float>
       </PresentationControls>
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={4} blur={2.4} />
     </>
   );
 }
